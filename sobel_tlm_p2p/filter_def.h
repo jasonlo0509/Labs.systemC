436967237ffd73ed4c896aff335302514c78e797
jasonlo0509 <tarzen0509@gmail.com>
2018-06-30 02:17:57 +0000

    lab2 run results

---
+++ sobel_tlm_p2p/filter_def.h
@@ -0,0 +1,18 @@
+#ifndef FILTER_DEF_H_
+#define FILTER_DEF_H_
+
+#define MASK_N 2
+#define MASK_X 3
+#define MASK_Y 3
+
+#define SOBEL_FILTER_R_ADDR (0x00000000)
+
+#define SOBEL_FILTER_RESULT_ADDR (0x00000004)
+
+typedef union word{
+	int sint;
+	unsigned int uint;
+	unsigned char uc[4];
+} word;
+
+#endif
